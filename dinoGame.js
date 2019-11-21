a5c8d0ce0b928dd896f68c8ecbe17618c6639038
GamerPsy <ducry.remy@gmail.com>
2019-11-21 21:59:35 +0100

    affichage du personnage avec mouvement haut, gauche et droite

--- dinoGame.js
+++ dinoGame.js
@@ -1,2 +1,48 @@
-console.log('hello Rémy')
-const game = new Phaser.Game(800, 800, Phaser.AUTO)
+const config = {
+    width : 800,
+    height : 600,
+    type: Phaser.AUTO,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 400}
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update,
+    }
+}
+
+const game = new Phaser.Game(config)
+let dino
+let pointage
+
+function preload()
+{
+    this.load.image('dino', 'assets/dino.png')
+    pointage = this.input.keyboard.createCursorKeys()
+}
+
+function create()
+{
+    dino = this.physics.add.image(200, 200, 'dino')
+    dino.body.collideWorldBounds = true
+
+}
+
+function update()
+{
+    dino.setVelocityX(0)
+
+    if(pointage.up.isDown){
+        dino.setVelocity(0, -300)
+    }
+    if(pointage.left.isDown){
+        dino.setVelocity(-200, 0)
+    }
+    if(pointage.right.isDown){
+        dino.setVelocity(200, 0)
+    }
+}
