827cea2163dbb23c2e5daa3da0281c5a210efc7b
GamerPsy <ducry.remy@gmail.com>
2019-11-22 23:57:06 +0100

    modif du update pour du switch case

--- src/dinoGame.js
+++ src/dinoGame.js
@@ -1,7 +1,7 @@
 const config = {
-    width : 800,
-    height : 600,
-    type: Phaser.CANVAS,
+    width: 800,
+    height: 600,
+    type: Phaser.AUTO,
     physics: {
         default: 'arcade',
         arcade: {
@@ -19,31 +19,30 @@ const game = new Phaser.Game(config)
 let dino
 let pointage
 
-function preload()
-{
-    this.load.image('dino', 'assets/dino.png', { frameWidth: 100, frameHeight: 100 })
+function preload() {
+    this.load.image('dino', 'assets/dino.png', {frameWidth: 100, frameHeight: 100})
     this.load.image('jungle', 'assets/jungle.png')
     pointage = this.input.keyboard.createCursorKeys()
 }
 
-function create()
-{
+function create() {
     this.add.image(0, 0, 'jungle').setOrigin(0, 0)
     dino = this.physics.add.image(50, 250, 'dino')
     dino.body.collideWorldBounds = true
 }
 
-function update()
-{
+function update() {
     dino.setVelocityX(0)
 
-    if(pointage.up.isDown){
-        dino.setVelocity(0, -300)
-    }
-    if(pointage.left.isDown){
-        dino.setVelocity(-200, 0)
-    }
-    if(pointage.right.isDown){
-        dino.setVelocity(200, 0)
+    switch (true) {
+        case pointage.up.isDown :
+            dino.setVelocity(0, -300)
+            break
+        case pointage.left.isDown :
+            dino.setVelocity(-200, 0)
+            break
+        case pointage.right.isDown :
+            dino.setVelocity(200, 0)
+            break
     }
 }
