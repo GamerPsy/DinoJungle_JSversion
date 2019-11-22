b8bd8622a9e253ce1ef9659fa622d79cd00d7eea
GamerPsy <ducry.remy@gmail.com>
2019-11-22 15:30:05 +0100

    creation dossier src

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/phaser/3.19.0/phaser.js"></script>
-    <script src="dinoGame.js"></script>
+    <script src="src/dinoGame.js"></script>
     <title>Dino Jungle</title>
 </head>
 <body class="container">
