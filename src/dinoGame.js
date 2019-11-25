66c9fd0ffb2ffd2b059a005b5ccace616094ae53
GamerPsy <ducry.remy@gmail.com>
2019-11-25 20:10:07 +0100

    ajout d'un oeuf avant interaction

--- src/dinoGame.js
+++ src/dinoGame.js
@@ -1,6 +1,6 @@
 const config = {
-    width: 800,
-    height: 600,
+    width : 800,
+    height : 600,
     type: Phaser.AUTO,
     physics: {
         default: 'arcade',
@@ -15,34 +15,39 @@ const config = {
     }
 }
 
-const game = new Phaser.Game(config)
+const GAME = new Phaser.Game(config)
 let dino
+let oeuf
 let pointage
 
-function preload() {
-    this.load.image('dino', 'assets/dino.png', {frameWidth: 100, frameHeight: 100})
+function preload()
+{
+    this.load.image('dino', 'assets/dino.png')
+    this.load.image('oeuf', 'assets/oeuf.png')
     this.load.image('jungle', 'assets/jungle.png')
     pointage = this.input.keyboard.createCursorKeys()
 }
 
-function create() {
-    this.add.image(0, 0, 'jungle').setOrigin(0, 0)
+function create()
+{
+    this.add.image(400, 300, 'jungle')
+    oeuf = this.physics.add.image(400, 200, 'oeuf')
+    oeuf.body.collideWorldBounds = true
     dino = this.physics.add.image(50, 250, 'dino')
     dino.body.collideWorldBounds = true
 }
 
-function update() {
+function update()
+{
     dino.setVelocityX(0)
 
-    switch (true) {
-        case pointage.up.isDown :
-            dino.setVelocity(0, -300)
-            break
-        case pointage.left.isDown :
-            dino.setVelocity(-200, 0)
-            break
-        case pointage.right.isDown :
-            dino.setVelocity(200, 0)
-            break
+    if(pointage.up.isDown){
+        dino.setVelocity(0, -300)
+    }
+    if(pointage.left.isDown){
+        dino.setVelocity(-200, 0)
+    }
+    if(pointage.right.isDown){
+        dino.setVelocity(200, 0)
     }
 }
